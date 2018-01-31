aac86f3ae17e2237d16ff1731c1a423ce70a6f8c
Nikhil Pathak <nikhilpathak926@gmail.com>
2018-01-31 19:04:35 +0530

    Solved Balance problem in Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,6 +4,10 @@ class Transaction < ApplicationRecord
 
   validates :transaction_type, presence: true, inclusion: { in: %w(credit debit), message: "invalid type" }
   validates :amount, format: { with: /\A[1-9]\d*(\.\d+)?\z/, message: "characters not allowed" }
-  validates :balance, format: { with: /\A[1-9]\d*(\.\d+)?\z/, message: "characters not allowed" }
+
+  before_create do
+    user = self.user
+    self.balance = user.balance - self.amount
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131103109) do
+ActiveRecord::Schema.define(version: 20180131124719) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20180131103109) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "balance"
+    t.float "balance", default: 0.0
   end
 
 end
