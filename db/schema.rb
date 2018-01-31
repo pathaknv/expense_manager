d033670310d009132e7e7a9e1a345f7d8e4fb200
Nikhil Pathak <nikhilpathak926@gmail.com>
2018-01-31 12:29:09 +0530

    USers Controller and Views

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131062107) do
+ActiveRecord::Schema.define(version: 20180131064616) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
