6c327457bdbaf4caa915d3cd633855137f0092ca
harish <harish@joshsoftware.com>
2018-01-31 16:40:47 +0530

    Balance methods added in transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,7 +5,9 @@ class TransactionsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    @transaction = @user.transactions.create(transaction_params)
+    @transaction_params_hash = transaction_params
+    @transaction_params_hash[:balance] = calculate_balance
+    @transaction = @user.transactions.create(@transaction_params_hash)
   end
 
   def index
@@ -13,10 +15,18 @@ class TransactionsController < ApplicationController
     @transactions = @user.transactions.all
   end
 
+
   private
+
   def transaction_params
-    get_current_balance params[:user_id]
     params.require(:transaction).permit(:transaction_type, :amount, :category, :user_id)
   end
 
+  def calculate_balance
+    balance = User.select(:balance).where(params.require(:transaction).permit(:user_id))
+    amount = params.require(:transaction).permit(:amount)
+    balance = balance - amount
+    User.where(:user_id => params.require(:transaction).permit(:user_id)).update(:balance => balance)
+    balance
+  end
 end
