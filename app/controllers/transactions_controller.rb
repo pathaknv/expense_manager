dd816c285acb51ebf701ccf0f26438d2e8033098
harish <harish@joshsoftware.com>
2018-01-31 15:35:04 +0530

    added transaction_controller methods

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,20 @@
 class TransactionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @transaction = @user.transactions.create(transaction_params)
+  end
+
+  def index
+    @user = User.find(params[:user_id])
+    @transactions = @user.transactions.all
+  end
+
+  private
+  def transaction_params
+    params.require(:transaction).permit(:transaction_type, :amount, :category, :user_id)
+  end
+  
 end
