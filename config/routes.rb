f82e6ff11c9f0219fedb140a141f4091821c1f01
harish <harish@joshsoftware.com>
2018-01-31 15:17:02 +0530

    added new transaction view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :transactions
   end
+
+  root 'transactions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
